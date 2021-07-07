a51f7075c88c0a4ccb42c214e02ff894804cdfa2
tanyagorn <tanyagorn.b@gmail.com>
2021-07-07 17:07:22 +0700

    Complete exercise_4

--- src/test/scala/MathUtilsTest.scala
+++ src/test/scala/MathUtilsTest.scala
@@ -12,4 +12,14 @@ class MathUtilsTest extends FunSpec {
       assert(!MathUtils.~=(0.1001, 0.1000))
     }
   }
+
+  describe("Find greatest common divisor of two numbers") {
+    it("should return 6 given 12 and 18") {
+      assert(MathUtils.GCD(12, 18) == 6)
+    }
+
+    it("should return 1 given 5 and 11") {
+      assert(MathUtils.GCD(5, 11) == 1)
+    }
+  }
 }
