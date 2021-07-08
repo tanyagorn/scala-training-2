b445595eb054c67972e8d9acbcdb9417128f8493
tanyagorn <tanyagorn.b@gmail.com>
2021-07-08 10:31:49 +0700

    Complete exercise_12

---
+++ src/test/scala/Exercise12Test.scala
@@ -0,0 +1,11 @@
+import org.scalatest.FunSpec
+
+class Exercise12Test extends FunSpec {
+
+  describe("Multiply numbers in a list with corresponding value in a map") {
+    val exercise12 = new Exercise12
+    assert(exercise12.multiplyNumbersInAListWithCorrespondingValueInAMap(List(1, 2, 3, 4), Map(1 -> 3, 3 -> 5)) == List(3, 15))
+    assert(exercise12.multiplyNumbersInAListWithCorrespondingValueInAMap(List(1, 2, 3, 4), Map(2 -> 3, 4 -> 10)) == List(6, 40))
+    assert(exercise12.multiplyNumbersInAListWithCorrespondingValueInAMap(List(1, 2, 3, 4), Map(5 -> 3)) == List())
+  }
+}
