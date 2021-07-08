aad6c314f064c22a6d7aec0846d53bfb34f09d80
tanyagorn <tanyagorn.b@gmail.com>
2021-07-08 14:31:17 +0700

    Remove comment

--- src/main/scala/TraitQueue.scala
+++ src/main/scala/TraitQueue.scala
@@ -1,7 +1,3 @@
-/**
- * Use a trait to define a generic queue of strings with 'put' and 'get' methods,
- * and create a class that implements it using an array. Include tests.
- */
 trait TraitQueue {
   def put(item: String): Array[String]
   def get(): String
